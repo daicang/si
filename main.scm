594dd5392b3345d177e65a39e93c55e85b957abc
daicang <daicang.cn@gmail.com>
2016-03-29 16:06:50 +0800

    Working on operator table and label-related code
    TODO:
    1. fill operator table
    2. add other labels

--- main.scm
+++ main.scm
@@ -1,11 +1,13 @@
 ;; main.scm
-;; TODO
 
-(define prompt "ss> ")
+(load "machine.scm")
+(load "utils.scm")
 
-(define (main-loop)
-  (let ((i (read)))
-    (display i)))
+;; Fill opterator table
+(define eceval-operations
+  (list (list 'self-evaluating? self-evaluating?)
+	(list 'read read)
+	;; TODO))
 
 ;; eval
 ;;
@@ -19,22 +21,32 @@
 ;; argl: for compound procedure
 ;; unev: unevaluated expression for compound procedure
 
+(define the-global-environment (setup-environment))
+
+(define (get-global-environment) the-global-environment)
+
 (define eceval
   (make-machine
    '(exp env val proc argl continue unev)
    eceval-operations
-   '(
-     read-eval-print-loop
-     ;; TODO
+   (
+    read-eval-print-loop
+      (perform (op initialize-stack))
+      (perform (op prompt-for-input) (const ";;; EC-Eval input:"))
+      (assign exp (op read))
+      (assign env (op get-global-environment))
+      (assign continue (label print-result))
+      (goto (label eval-dispatch))
+    print-result
+      (perform (op announce-output) (const ";;; EC-Eval value:"))
+      (perform (op user-print) (reg val))
+      (goto (label read-eval-print-loop))
+     
+     
      )))
 
-(define eceval-operations
-  (list (list 'self-evaluating? self-evaluating)
-	))
 
 
-(define (eval exp env)
-  (define (eval-dispatch exp)
     (test (op self-evaluating?) (reg exp))
     (branch (label ev-self-eval))
     
@@ -62,8 +74,7 @@
     (test (op application?) (reg exp))
     (branch (label ev-application))
 
-    (goto (label unknown-expression-type))))
+    (goto (label unknown-expression-type))
 
-(define the-global-environment (setup-environment))
 
 (start eceval)
