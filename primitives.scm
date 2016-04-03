85fa8ff0a803ea1c7516bd22612d22a5eaff5be1
daicang <daicang.cn@gmail.com>
2016-04-03 21:21:58 +0800

    Add primitives.scm

---
+++ primitives.scm
@@ -0,0 +1,29 @@
+;; primitives.scm
+;;
+;; ('primitive <primitive-proc> <args>)
+
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc) (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car car)
+	(list 'cdr cdr)
+	(list 'cons cons)
+	(list 'null? null?)
+	(list '+ +)
+	(list '- -)
+	(list '* *)
+	(list '/ /)
+	(list '= =)
+	(list 'display display)))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc)
+	 (list 'primitive (cadr proc)))
+       primitive-procedures))
